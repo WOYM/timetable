9db4f9c8ac456848e28a779cec23226cd598bbb9
tihansen <tihansen@uni-bremen.de>
2014-11-05 09:52:03 +0100

    Added ContextMenu for Datatable

--- src/main/java/com/woym/controller/TeacherController.java
+++ src/main/java/com/woym/controller/TeacherController.java
@@ -3,6 +3,7 @@ package com.woym.controller;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
@@ -23,6 +24,8 @@ public class TeacherController implements Serializable {
 	private static final long serialVersionUID = -2341971622906815080L;
 	ArrayList<Teacher> teachers = new ArrayList<>();
 
+	private Teacher selectedTeacher;
+
 	@PostConstruct
 	public void init() {
 		Teacher teacher1 = new Teacher();
@@ -30,13 +33,13 @@ public class TeacherController implements Serializable {
 		teacher1.setSymbol("MEY");
 		teacher1.setWeekhours(40);
 		teachers.add(teacher1);
-		
+
 		Teacher teacher2 = new Teacher();
 		teacher2.setName("Herr Schulz");
 		teacher2.setSymbol("SCH");
 		teacher2.setWeekhours(40);
 		teachers.add(teacher2);
-		
+
 		Teacher teacher3 = new Teacher();
 		teacher3.setName("Herr Müller");
 		teacher3.setSymbol("MUE");
@@ -49,52 +52,65 @@ public class TeacherController implements Serializable {
 	}
 
 	public void addTeacherDialog() {
-		
-        Map<String,Object> options = new HashMap<String, Object>();
-        options.put("modal", true);
-        options.put("draggable", false);
-        options.put("resizable", false);
-        options.put("contentHeight", 400);
-        options.put("contentWidth", 600);
-		
+
+		Map<String, Object> options = new HashMap<String, Object>();
+		options.put("modal", true);
+		options.put("draggable", false);
+		options.put("resizable", false);
+		options.put("contentHeight", 400);
+		options.put("contentWidth", 600);
+
 		RequestContext rc = RequestContext.getCurrentInstance();
 		rc.openDialog("manageTeachersDialog", options, null);
 	}
-	
+
 	public void editTeacherDialog() {
-		
-        Map<String,Object> options = new HashMap<String, Object>();
-        options.put("modal", true);
-        options.put("draggable", false);
-        options.put("resizable", false);
-        options.put("contentHeight", 400);
-        options.put("contentWidth", 600);
+
+		Map<String, Object> options = new HashMap<String, Object>();
+		options.put("modal", true);
+		options.put("draggable", false);
+		options.put("resizable", false);
+		options.put("contentHeight", 400);
+		options.put("contentWidth", 600);
 		
 		RequestContext rc = RequestContext.getCurrentInstance();
 		rc.openDialog("editTeacherDialog", options, null);
 	}
-	
+
 	public void onTeacherAdded(SelectEvent event) {
-		
+
 		Teacher teacher = (Teacher) event.getObject();
-		
-		for(Teacher currentTeacher : teachers) {
-			if(currentTeacher.getSymbol().equals(teacher.getSymbol())) {
-		        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Lehrer existiert bereits", "");
-		        FacesContext.getCurrentInstance().addMessage(null, message);
-		        return;
+
+		for (Teacher currentTeacher : teachers) {
+			if (currentTeacher.getSymbol().equals(teacher.getSymbol())) {
+				FacesMessage message = new FacesMessage(
+						FacesMessage.SEVERITY_ERROR,
+						"Lehrer existiert bereits", "");
+				FacesContext.getCurrentInstance().addMessage(null, message);
+				return;
 			}
 		}
-		
+
 		teachers.add(teacher);
-		
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Lehrer hinzugefügt", "");
-        FacesContext.getCurrentInstance().addMessage(null, message);
+
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+				"Lehrer hinzugefügt", "");
+		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
-	
-	public void deleteTeacher(Teacher teacher) {
-		if(teachers.contains(teacher)) {
-			teachers.remove(teacher);
+
+	public void deleteTeacher() {
+		if (selectedTeacher != null) {
+			if (teachers.contains(selectedTeacher)) {
+				teachers.remove(selectedTeacher);
+			}
 		}
+	}
+
+	public Teacher getSelectedTeacher() {
+		return selectedTeacher;
+	}
+
+	public void setSelectedTeacher(Teacher selectedTeacher) {
+		this.selectedTeacher = selectedTeacher;
 	}
 }
