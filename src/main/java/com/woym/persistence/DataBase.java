80584d10aae773c78c8ac02639322adbfabe21ef
Adrian Lück <alueck@uni-bremen.de>
2014-11-08 00:37:58 +0100

    Die Datenbank wird nun geladen, bevor die Startseite gerendert wird.

--- src/main/java/com/woym/persistence/DataBase.java
+++ src/main/java/com/woym/persistence/DataBase.java
@@ -1,25 +1,30 @@
 package com.woym.persistence;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ComponentSystemEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceException;
 
-public abstract class DataBase {
+@ManagedBean
+@SessionScoped
+public class DataBase {
 
 	static EntityManager entityManager;
 
-	static{
-		try {
-			EntityManagerFactory factory;
-			factory = Persistence.createEntityManagerFactory("timetable");
-			entityManager = factory.createEntityManager();
-		} catch (Exception e) {
-			throw new PersistenceException(
-					"Could not initialize persistence component: "
-							+ e.getMessage());
+	public void setUp(ComponentSystemEvent event) {
+		if (entityManager == null) {
+			try {
+				EntityManagerFactory factory;
+				factory = Persistence.createEntityManagerFactory("timetable");
+				entityManager = factory.createEntityManager();
+			} catch (Exception e) {
+				throw new PersistenceException(
+						"Could not initialize persistence component: "
+								+ e.getMessage());
+			}
 		}
-
 	}
-
 }
