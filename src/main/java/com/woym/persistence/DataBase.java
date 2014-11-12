008e5af4f95fa220ef29a06b372cebe963e0abfd
Tim Hansen <tihansen@uni-bremen.de>
2014-11-12 10:58:56 +0100

    Added Loggerconfiguration

--- src/main/java/com/woym/persistence/DataBase.java
+++ src/main/java/com/woym/persistence/DataBase.java
@@ -10,20 +10,27 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @ManagedBean
 @SessionScoped
 public class DataBase implements Serializable {
 	
 	private static final long serialVersionUID = 7717982039237763919L;
 	
+	private static Logger logger = LogManager.getLogger("DataBase");
+	
 	private static EntityManager ENTITY_MANAGER;
 	
 	public void setUp(ComponentSystemEvent event) {
 		if (ENTITY_MANAGER == null) {
+			logger.info("Establishing database-connection...");
 			try {
 				EntityManagerFactory factory;
 				factory = Persistence.createEntityManagerFactory("timetable");
 				ENTITY_MANAGER = factory.createEntityManager();
+				logger.info("Connection established.");
 			} catch (Exception e) {
 				throw new PersistenceException(
 						"Could not initialize persistence component: "
