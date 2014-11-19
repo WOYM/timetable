8cd0aff87d4d3d6518af255a332c65bce7c12859
tihansen <tihansen@uni-bremen.de>
2014-11-19 10:13:28 +0100

    Fixed J-Unit Setup / Added PowerMock

---
+++ src/test/java/org/woym/controller/manage/TeacherControllerTest.java
@@ -0,0 +1,15 @@
+package org.woym.controller.manage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TeacherControllerTest {
+
+	@Test
+	public void test() {
+		int i = 1 + 1;
+		assertEquals(2, i);
+	}
+
+}
