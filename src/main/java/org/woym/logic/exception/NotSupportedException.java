0ec9e0b0ccb2f7f38be4a7ff4164a66caf728243
WhateverItoldYou <jur_sch@uni-bremen.de>
2014-12-14 13:08:09 +0100

    Revert "#78"
    This reverts commit 814d5cfe36b785822b351572317467e09a4b9c30.

--- src/main/java/org/woym/logic/exception/NotSupportedException.java
+++ src/main/java/org/woym/logic/exception/NotSupportedException.java
@@ -4,15 +4,13 @@
 package org.woym.logic.exception;
 
 /**
- * Exception zur anzeige unfertiger oder nicht vorhandener funktionalität
- * 
- * @author JurSch
+ * @author Whatever
  *
  */
 public class NotSupportedException extends RuntimeException{
 
 	/**
-	 * Automatisch generiert
+	 * 
 	 */
 	private static final long serialVersionUID = -7072619228990446071L;
 	
