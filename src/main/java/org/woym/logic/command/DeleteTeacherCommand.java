bb2642d4b037053bd59b51012c7fcc751d754d31
WhateverItoldYou <jur_sch@uni-bremen.de>
2014-12-14 13:20:22 +0100

    Implementierung erster Commands #56
    TeacherCommands added

---
+++ src/main/java/org/woym/logic/command/DeleteTeacherCommand.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.woym.logic.command;
+
+import org.woym.spec.logic.ICommand;
+import org.woym.spec.logic.IStatus;
+
+/**
+ * @author JurSch
+ *
+ */
+public class DeleteTeacherCommand implements ICommand {
+
+	@Override
+	public IStatus execute() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public IStatus undo() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public IStatus redo() {
+		throw new UnsupportedOperationException();
+	}
+
+}
