d515a3e417ed169b0a08cb704ac479425cc09c4d
WhateverItoldYou <jur_sch@uni-bremen.de>
2014-12-14 12:47:32 +0100

    TeacherCommands #56

---
+++ src/main/java/org/woym/logic/command/UpdateTeacherCommand.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.woym.logic.command;
+
+import org.woym.logic.exception.NotSupportedException;
+import org.woym.spec.logic.ICommand;
+import org.woym.spec.logic.IStatus;
+
+/**
+ * @author JurSch
+ *
+ */
+public class UpdateTeacherCommand implements ICommand {
+
+	@Override
+	public IStatus execute() {
+		throw new NotSupportedException();
+	}
+
+	@Override
+	public IStatus undo() {
+		throw new NotSupportedException();
+	}
+
+	@Override
+	public IStatus redo() {
+		throw new NotSupportedException();
+	}
+
+}
