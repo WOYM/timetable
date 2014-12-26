0d596fd82b70225ea96d2bcae88731d1e32b33d1
WhateverItoldYou <jur_sch@uni-bremen.de>
2014-12-26 16:36:24 +0100

    mid commit

---
+++ src/test/java/org/woym/logic/command/TestCommandhandler.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.woym.logic.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author JurSch
+ *
+ */
+public class TestCommandhandler {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
